36902f6700f7db4d2c735ccf02eb36bb8c228be6
valdeirmatias <valdeirmatias@live.com>
2022-04-17 13:15:06 -0300

    formulario-mascaras-validacao

--- js/custom.js
+++ js/custom.js
@@ -90,4 +90,20 @@ $(document).ready(function () {
       );
     }
   });
+
+  /*
+   * Ouvinte de eventos .nav-modal-open
+   */
+  $(".nav-modal-open").on("click", function (e) {
+    e.preventDefault();
+
+    let elem = $(this).attr("rel");
+
+    $(".modal-body").html($("#" + elem).html());
+    $(".modal-header h5.modal-title").html($(this).text());
+
+    let myModal = new bootstrap.Modal($("#modelId"));
+
+    myModal.show();
+  });
 });
